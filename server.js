603dfd3eb480e9a318bbad99b51b551f627f7756
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2024-02-15 18:43:40 -0500

    Update server.js
    removed password

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Faust,0013!',
+    password: '<insert your password>',
     database: 'employ_db'
 });
 
